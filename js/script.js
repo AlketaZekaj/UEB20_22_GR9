f61f721b983c8c92074e4832f38d79efa784b645
Anjeza Gashi <anjeza.gashi03@gmail.com>
2023-01-18 20:36:08 +0100

    small changes

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 let videoBtn = document.querySelectorAll('.vid-btn');
 
-window.onscroll = () =>{
+window.onscroll = () => {
     searchBtn.classList.remove('fa-times');
     searchBar.classList.remove('active');
     menu.classList.remove('fa-times');
@@ -15,26 +15,26 @@ window.onscroll = () =>{
     loginForm.classList.remove('active');
 }
 
-menu.addEventListener('click', () =>{
+menu.addEventListener('click', () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 });
 
-searchBtn.addEventListener('click', () =>{
+searchBtn.addEventListener('click', () => {
     searchBtn.classList.toggle('fa-times');
     searchBar.classList.toggle('active');
 });
 
-formBtn.addEventListener('click', () =>{
+formBtn.addEventListener('click', () => {
     loginForm.classList.add('active');
 });
 
-formClose.addEventListener('click', () =>{
+formClose.addEventListener('click', () => {
     loginForm.classList.remove('active');
 });
 
-videoBtn.forEach(btn =>{
-    btn.addEventListener('click', ()=>{
+videoBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
         document.querySelector('.controls .active').classList.remove('active');
         btn.classList.add('active');
         let src = btn.getAttribute('data-src');
@@ -44,20 +44,24 @@ videoBtn.forEach(btn =>{
 
 var swiper = new Swiper(".review-slider", {
     spaceBetween: 20,
-    loop:true,
+    loop: true,
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
     },
     breakpoints: {
         640: {
-          slidesPerView: 1,
+            slidesPerView: 1,
         },
         768: {
-          slidesPerView: 2,
+            slidesPerView: 2,
         },
         1024: {
-          slidesPerView: 3,
+            slidesPerView: 3,
         },
     },
 });
+
+function btnMouseEnterEvent() {
+    console.warn("We're directing you to the booking page!");
+}
