b8dfc0aecf1fb688ae9b3e5004290b9190a7b94a
Anjeza Gashi <anjeza.gashi03@gmail.com>
2023-01-20 19:49:32 +0100

    Krijimi i objekteve

--- js/script.js
+++ js/script.js
@@ -61,6 +61,48 @@ function validateEmail(email) {
 }
 //end
 
+
+//krijimi i objekteve
+function Udhëtimi(destination, startDate, endDate) {
+    this.destination = destination;
+    this.startDate = startDate;
+    this.endDate = endDate;
+}
+
+// Krijimi i një instance të objektit Udhëtimi
+let udhetimi1 = new Udhëtimi("Paris", "01/01/2023", "01/05/2023");
+
+let udhetimi2 = new Udhëtimi("Roma", "06/02/2023", "06/05/2023");
+
+let udhetimi3 = new Udhëtimi("Barcelona", "03/23/2023", "03/27/2023");
+
+let udhetimi4 = new Udhëtimi("London", "04/12/2023", "04/18/2023");
+
+let udhetimi5 = new Udhëtimi("Maldives", "05/15/2023", "05/20/2023");
+
+
+document.getElementById("destinacioni").innerHTML = udhetimi1.destination;
+document.getElementById("data-fillimit").innerHTML = udhetimi1.startDate;
+document.getElementById("data-mbarimit").innerHTML = udhetimi1.endDate;
+
+document.getElementById("destinacioni1").innerHTML = udhetimi2.destination;
+document.getElementById("data-fillimit1").innerHTML = udhetimi2.startDate;
+document.getElementById("data-mbarimit1").innerHTML = udhetimi2.endDate;
+
+document.getElementById("destinacioni2").innerHTML = udhetimi3.destination;
+document.getElementById("data-fillimit2").innerHTML = udhetimi3.startDate;
+document.getElementById("data-mbarimit2").innerHTML = udhetimi3.endDate;
+
+document.getElementById("destinacioni3").innerHTML = udhetimi4.destination;
+document.getElementById("data-fillimit3").innerHTML = udhetimi4.startDate;
+document.getElementById("data-mbarimit3").innerHTML = udhetimi4.endDate;
+
+document.getElementById("destinacioni4").innerHTML = udhetimi5.destination;
+document.getElementById("data-fillimit4").innerHTML = udhetimi5.startDate;
+document.getElementById("data-mbarimit4").innerHTML = udhetimi5.endDate;
+
+//end
+
 formBtn.addEventListener('click', () => {
     loginForm.classList.add('active');
 });
